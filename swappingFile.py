58549048d3b15a70d83450c683e5b5a6713cd18e
AnshulKumarasamy <68804325+AnshulKumarasamy@users.noreply.github.com>
2022-02-15 11:34:53 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,10 @@
+def swapFileData():
+    file1 = "F:/Coding/HW/Python/C98/sample1.txt"
+    data_a = open(file1,'w')
+    file1.write(data_b)
+
+    file2 = "F:/Coding/HW/Python/C98/sample2.txt"
+    data_b = open(file2,'w')
+    file2.write(data_a)
+
+swapFileData()
